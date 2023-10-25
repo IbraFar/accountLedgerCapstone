f436a00e4224076808a805d0c3e83b3746c3c862
ifarhane <ifarhane@vassar.edu>
2023-10-25 13:27:55 -0400

    Initial Java commit

--- src/com/pluralsight/LedgerApp.java
+++ src/com/pluralsight/LedgerApp.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
 
 public class LedgerApp {
+
+    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    private static final String FILE_NAME = "transactions.csv";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_FORMAT = "HH:mm:ss";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_FORMAT);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Manage the stack frames
+        while (running) {
+            printHomeScreen();
+
+
+        }
     }
+
+    public static void printHomeScreen(){
+        System.out.println("Welcome to TransactionApp");
+        System.out.println("Choose an option:");
+        System.out.println("D) Add Deposit");
+        System.out.println("P) Make Payment (Debit)");
+        System.out.println("L) Ledger");
+        System.out.println("X) Exit");
+    }
+
+    public static void getStringInput(Scanner scanner){
+
+    }
+
 }

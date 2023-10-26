8947c2c608295bb531e3d0e207bfbbb52c85b7f8
ifarhane <ifarhane@vassar.edu>
2023-10-26 08:36:15 -0400

    Added Home Screen and FileReader/Writer

--- src/com/pluralsight/FileManager.java
+++ src/com/pluralsight/FileManager.java
@@ -1,7 +1,16 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class FileManager {
-
-
+    private ArrayList<Transaction> transactionArrayList= new ArrayList<>();
+    String customFileName = "SAMPLE_FILE_NAME";
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(customFileName));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(customFileName));
 
 }
--- src/com/pluralsight/LedgerApp.java
+++ src/com/pluralsight/LedgerApp.java
@@ -1,23 +1,39 @@
 package com.pluralsight;
 
-public class LedgerApp {
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+public class LedgerApp {
+    private static final ArrayList<Transaction> transactions = new ArrayList<Transaction>();
     private static final String FILE_NAME = "transactions.csv";
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private static final String TIME_FORMAT = "HH:mm:ss";
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_FORMAT);
+
     public static void main(String[] args) {
         //Manage the stack frames
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
         while (running) {
-            printHomeScreen();
+
+            printHomeScreenOptions();
 
 
+            homeScreenSwitchCase(scanner);
+
+
+
+            running = false;
         }
     }
 
-    public static void printHomeScreen(){
+
+    public static void printHomeScreenOptions(){
         System.out.println("Welcome to TransactionApp");
         System.out.println("Choose an option:");
         System.out.println("D) Add Deposit");
@@ -26,8 +42,47 @@ public class LedgerApp {
         System.out.println("X) Exit");
     }
 
-    public static void getStringInput(Scanner scanner){
-
+    public static String getStringInput(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine().trim();
     }
 
+    public static Character getCharInput(Scanner scanner, String prompt){
+        System.out.println(prompt);
+        return scanner.nextLine().charAt(0);
+    }
+
+    public static BigDecimal getBigDecimalInput(Scanner scanner,String prompt){
+        System.out.println(prompt);
+        return scanner.nextBigDecimal();
+    }
+
+    public static void homeScreenSwitchCase(Scanner scanner){
+
+        Character input = getCharInput(scanner, "Please enter the initial of your choice: \n");
+
+        switch (Character.toUpperCase(input)) {
+            case 'D':
+                addDeposit(scanner);
+                break;
+            case 'P':
+                //addPayment(scanner);
+                break;
+            case 'L':
+                //ledgerMenu(scanner);
+                break;
+            case 'X':
+                //running = false;
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+    }
+
+    public static void addDeposit(Scanner scanner){
+
+        getStringInput(scanner, "Sample deposit method code");
+    }
 }
+
